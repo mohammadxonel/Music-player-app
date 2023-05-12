b235afbd4611260614b626de079a596709b06ffd
mohammadxxali <ali_mohammad@t-online.de>
2023-05-12 06:23:17 +0200

    add function prevsong

--- app.js
+++ app.js
@@ -39,3 +39,26 @@ function pauseSong() {
     music.pause();
 }
 
+// Play or Pause Event Listener //
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM //
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+// Current Song //
+let songIndex = 0;
+
+// Previous Song //
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
